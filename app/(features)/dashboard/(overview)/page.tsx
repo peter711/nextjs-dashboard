4248a41e29266bc7d9a70184e6a14fc6506b31b9
Piotr Falkowski <piotrfalkowski@MacBook-Pro-Piotr.local>
2024-06-15 10:06:34 +0200

    Added dummy pages

--- app/(features)/dashboard/(overview)/page.tsx
+++ app/(features)/dashboard/(overview)/page.tsx
@@ -1,15 +1,14 @@
 import { Suspense } from 'react';
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
+
 
 export default async function Page() {
   return (
--- app/(features)/dummy/page.tsx
+++ app/(features)/dummy/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
+async function getData() {
+  const res = await fetch('https://dummy.restapiexample.com/api/v1/employees/');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    console.error(res.statusText);
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+  console.log(data);
   return <p>This is dummy page</p>;
 }
